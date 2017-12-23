6ed1aae66a029df43bc321ca51988929c4c7ad78
DanielGreene101 <d.b.greene101@gmail.com>
2017-12-22 20:45:26 -0600

    i was bored

---
+++ friends.py
@@ -0,0 +1,18 @@
+import random
+import sys
+import os
+
+print("hello world")
+
+friend = ["Rachael", "Ross", "Chandler", "Phoebe", "Joey", "Monica"]
+print friend
+
+print('Best of the friends = ', friend[2])
+
+friend[2] = "Ross"
+
+print('The worst of the friends is', friend[2])
+
+friend[2] = "Chandler"
+
+print("The funny friends are", friend[2:5])
