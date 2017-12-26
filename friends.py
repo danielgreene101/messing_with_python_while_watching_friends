c88c7cc30204ed8506455d71c2682d1a35c8a969
DanielGreene101 <d.b.greene101@gmail.com>
2017-12-26 08:46:07 -0600

    finished lists, moving on to tupils

--- friends.py
+++ friends.py
@@ -20,3 +20,25 @@ print("The funny friends are", friend[2:5])
 print(random.choice(friend))
 
 print(random.choice(friend), "is at the door")
+
+print(friend)
+
+my_friends = ["Noah", "Bobby", "Emily", "Marcella"]
+
+all_friends = [friend, my_friends]
+
+print(all_friends)
+
+print(all_friends[1][0])
+
+my_friends.append("Autum")
+print(my_friends)
+
+del my_friends[4]
+
+print(my_friends)
+
+print(len(all_friends))
+
+all_friends_again = my_friends + friend
+print(len(all_friends_again))
